840547e83b852e3b65c04acbed8a1150a682e4f0
Lazyllama <me@lazyllama.xyz>
2026-06-03 23:35:40 +0000

    fix(shop): wishlist rework with turbo stream

--- app/controllers/shop/wishlists_controller.rb
+++ app/controllers/shop/wishlists_controller.rb
@@ -1,15 +1,28 @@
 # frozen_string_literal: true
 
 class Shop::WishlistsController < Shop::BaseController
+  private def render_wishlist_widget
+    render turbo_stream: turbo_stream.replace(
+    "discover-rail-wishlist",
+      render_to_string(
+        DiscoverRail::ShopWishlistWidget.new(
+          user: current_user,
+          context: { user_balance: current_user&.cached_balance || 0 }
+        ),
+        layout: false
+      )
+    )
+  end
+
   def create
     authorize :shop
     current_user.shop_wishlists.find_or_create_by!(shop_item_id: params[:id])
-    render json: { wishlisted: true }
+    render_wishlist_widget
   end
 
   def destroy
     authorize :shop
     current_user.shop_wishlists.where(shop_item_id: params[:id]).destroy_all
-    render json: { wishlisted: false }
+    render_wishlist_widget
   end
 end
--- app/javascript/controllers/shop_wishlist_controller.js
+++ app/javascript/controllers/shop_wishlist_controller.js
@@ -19,11 +19,47 @@ export default class extends Controller {
       method,
       headers: {
         "X-CSRF-Token": csrfToken,
-        Accept: "application/json",
+        Accept: "text/vnd.turbo-stream.html",
       },
     }).then((r) => {
       if (!r.ok) this.wishlistedValue = wasWishlisted;
+      else {
+        r.text().then((html) => Turbo.renderStreamMessage(html));
+        this.dispatch("changed", { detail: { itemId: this.itemIdValue, wishlisted: this.wishlistedValue } });
+      }
     });
+  }
+
+  remove(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.wishlistedValue = false;
+
+    const csrfToken = document.querySelector(
+      'meta[name="csrf-token"]',
+    )?.content;
+
+    fetch(`/shop/wishlists/${this.itemIdValue}`, {
+      method: "DELETE",
+      headers: {
+        "X-CSRF-Token": csrfToken,
+        Accept: "text/vnd.turbo-stream.html",
+      },
+    }).then((r) => {
+      if (!r.ok) this.wishlistedValue = true;
+      else {
+        r.text().then((html) => Turbo.renderStreamMessage(html))
+        this.dispatch("changed", { detail: { itemId: this.itemIdValue, wishlisted: false } });
+      }
+    });
+  }
+
+  sync(event) {
+    const { itemId, wishlisted } = event.detail;
+    if (itemId !== this.itemIdValue) return;
+
+    this.wishlistedValue = wishlisted;
   }
 
   wishlistedValueChanged() {
