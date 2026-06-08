d6e41049c9430c62671dbdcaf94e483159968648
Abdallah Ebrahim (dracula) <abdallah.ebrahim.official@gmail.com>
2026-06-08 07:19:52 +0300

    chore(seeds): generate development users

--- db/development_seed/config.rb
+++ db/development_seed/config.rb
@@ -1,4 +1,6 @@
 module DevelopmentSeed
   module Config
+    USER_COUNT = 20
+    RANDOM_SEED = 42
   end
 end
--- db/development_seed/runner.rb
+++ db/development_seed/runner.rb
@@ -1,5 +1,6 @@
 require_relative "config"
 require_relative "helpers"
+require_relative "users"
 
 module DevelopmentSeed
   class Runner
@@ -11,8 +12,28 @@ module DevelopmentSeed
     end
 
     def call
-      log "Starting development seed..."
-      log "Development seed framework ready."
+      log "Starting development community seed..."
+      
+      cleanup
+      
+      users = DevelopmentSeed::Users.call
+      
+      log "Successfully generated #{users.count} users."
+      log "Development community seed complete!"
+    end
+
+    private
+
+    def cleanup
+      progress "Cleaning up existing community data"
+      
+      admin_email = "kartikey@hackclub.com"
+      
+      User.where.not(email: admin_email).destroy_all
+      
+      Project.where.not(id: Project::Membership.where(user: User.find_by(email: admin_email)).select(:project_id)).destroy_all
+      
+      done
     end
   end
 end
