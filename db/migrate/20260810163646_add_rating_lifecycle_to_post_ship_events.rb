cbca7d02ec9f709a928f9316d39be6e5ed6a2cee
Kartikey Chauhan <me@cskartikey.dev>
2026-08-11 00:26:56 +0530

    fix(migration): add if_not_exists option to columns and indexes in post_ship_events migration

--- db/migrate/20260810163646_add_rating_lifecycle_to_post_ship_events.rb
+++ db/migrate/20260810163646_add_rating_lifecycle_to_post_ship_events.rb
@@ -2,13 +2,13 @@ class AddRatingLifecycleToPostShipEvents < ActiveRecord::Migration[8.1]
   disable_ddl_transaction!
 
   def change
-    add_column :post_ship_events, :voting_started_at, :datetime
-    add_column :post_ship_events, :voting_completed_at, :datetime
-    add_column :post_ship_events, :paid_at, :datetime
-    add_column :post_ship_events, :lifecycle_data_quality, :string
+    add_column :post_ship_events, :voting_started_at, :datetime, if_not_exists: true
+    add_column :post_ship_events, :voting_completed_at, :datetime, if_not_exists: true
+    add_column :post_ship_events, :paid_at, :datetime, if_not_exists: true
+    add_column :post_ship_events, :lifecycle_data_quality, :string, if_not_exists: true
 
-    add_index :post_ship_events, :voting_started_at, algorithm: :concurrently
-    add_index :post_ship_events, :voting_completed_at, algorithm: :concurrently
-    add_index :post_ship_events, :paid_at, algorithm: :concurrently
+    add_index :post_ship_events, :voting_started_at, algorithm: :concurrently, if_not_exists: true
+    add_index :post_ship_events, :voting_completed_at, algorithm: :concurrently, if_not_exists: true
+    add_index :post_ship_events, :paid_at, algorithm: :concurrently, if_not_exists: true
   end
 end
